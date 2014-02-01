1efe6ce1f933e7db578df99dad221310d168f5e0
eb-drocha <diogo.rocha@emailbidding.com>
2014-02-01 18:50:07 +0000

    Removed socket.io from server and client: now server uses ws node module and client uses native HTML5 API

--- client/js/MySocket.js
+++ client/js/MySocket.js
@@ -1,4 +1,4 @@
-function MySocket(host, port, receiveHandler) {
+/*function MySocket(host, port, receiveHandler) {
     this.socket = io.connect(host + ':' + port);
 
     // Add a connect listener
@@ -20,6 +20,33 @@ function MySocket(host, port, receiveHandler) {
 
 MySocket.prototype.sendMessage = function (msg) {
     this.socket.send(msg);
+}*/
+
+
+function MySocket(host, port, receiveHandler) {
+    this.socket = new WebSocket('ws://' + host + ':' + port);
+
+    // When the connection is open, send some data to the server
+    this.socket.onopen = function () {
+        console.log('Client has connected to the server!');
+    };
+
+    // Log errors
+    this.socket.onerror = function (error) {
+        console.log('WebSocket Error ' + error);
+    };
+
+    // Log messages from the server
+    this.socket.onmessage = function (e) {
+        console.log('Received a message from the server!', e.data);
+        receiveHandler(e.data);
+    };
 }
+
+MySocket.prototype.sendMessage = function (msg) {
+    this.socket.send(msg);
+}
+
+
 
 
--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-// Require HTTP module (to start server) and Socket.IO
-var http = require('http'), io = require('socket.io');
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 8080});
 
-// Start the server at port 8080
-var server = http.createServer(function (req, res) {
+console.log('Server Started');
 
-    // Send HTML headers and message
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello Socket Lover!</h1>');
-});
-server.listen(8080);
+wss.on('connection', function(ws) {
+    console.log('New connection');
 
-// Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
-
-// Add a connect listener
-socket.on('connection', function (client) {
-
-    // Success!  Now listen to messages to be received
-    client.on('message',function (event) {
-        console.log('Received message from client!', event);
-        client.send(event);
-    });
-
-    client.on('disconnect',function () {
-        clearInterval(10);
-        console.log('Server has disconnected');
+    // message from client listener
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        ws.send(message);
     });
 });
