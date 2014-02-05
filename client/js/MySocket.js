39e79e3136338d0363187699ae7fb6b703894b72
eb-drocha <diogo.rocha@emailbidding.com>
2014-02-05 23:06:29 +0000

    testing on sockets

--- client/js/MySocket.js
+++ client/js/MySocket.js
@@ -1,28 +1,3 @@
-/*function MySocket(host, port, receiveHandler) {
-    this.socket = io.connect(host + ':' + port);
-
-    // Add a connect listener
-    this.socket.on('connect',function() {
-        console.log('Client has connected to the server!');
-    });
-
-    // Add a receibe listener
-    this.socket.on('message',function(msg) {
-        console.log('Received a message from the server!', msg);
-        receiveHandler(msg);
-    });
-
-    // Add a disconnect listener
-    this.socket.on('disconnect',function() {
-        console.log('The client has disconnected!');
-    });
-}
-
-MySocket.prototype.sendMessage = function (msg) {
-    this.socket.send(msg);
-}*/
-
-
 function MySocket(host, port, receiveHandler) {
     this.socket = new WebSocket('ws://' + host + ':' + port);
 
@@ -41,6 +16,12 @@ function MySocket(host, port, receiveHandler) {
         console.log('Received a message from the server!', e.data);
         receiveHandler(e.data);
     };
+
+    /*return {
+        sendMessage: function (msg) {
+            this.socket.send(msg);
+        }
+    }*/
 }
 
 MySocket.prototype.sendMessage = function (msg) {
